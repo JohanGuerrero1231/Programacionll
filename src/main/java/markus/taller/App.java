c38b995f43c57d49bbbc32e8c8061f39d5705299
JohanGuerrero1231 <142913523+JohanGuerrero1231@users.noreply.github.com>
2023-08-31 22:23:44 -0500

    Asistida

---
+++ src/main/java/markus/taller/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package markus.taller;
+
+
+
+/**
+ *
+ * @author johan
+ */
+public class App {
+
+    public static void main(String[] args) {
+        interfaz in = new interfaz();
+        in.setVisible(true);
+    }
+}
